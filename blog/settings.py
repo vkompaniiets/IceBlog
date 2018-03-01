6dea878d284807bb4898f22762cbbca1132988a3
vkompaniiets <haden921@gmail.com>
2018-03-01 07:13:30 +0200

    Fix edit post, add TinyMCE for textarea

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'account',
     'post',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- post/models.py
+++ post/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.db.models import Model, ForeignKey, CharField, BooleanField, DateTimeField, TextField, DO_NOTHING
+from tinymce.models import HTMLField
 
 
 class Post(Model):
     created_by = ForeignKey(User, blank=True, null=True, on_delete=DO_NOTHING)
     name = CharField(max_length=50)
     published = BooleanField(default=False)
-    description = TextField(blank=True, null=True, default=None)
+    description = HTMLField()
     created = DateTimeField(auto_now_add=True, auto_now=False)
     updated = DateTimeField(auto_now_add=False, auto_now=True)
